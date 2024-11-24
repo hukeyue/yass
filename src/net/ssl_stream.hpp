2fbed745d1643e0daad1eff79e6738c941dbc9be
Keeyou <keeyou-cn@outlook.com>
2024-11-25 07:41:36 +0800

    net: close sockt early in failure

--- src/net/ssl_stream.hpp
+++ src/net/ssl_stream.hpp
@@ -89,7 +89,7 @@ class ssl_stream : public stream {
       asio::error_code ec;
       if (rv < 0) {
         ec = asio::error::connection_refused;
-        on_async_connected(channel, ec);
+        stream::on_async_connected(channel, ec);
         return;
       }
 
