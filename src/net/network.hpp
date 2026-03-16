74834e68fb2a2cdfb01d2475ac7877074a179691
Keyue Hu <hukeyue@vip.163.com>
2026-03-16 14:11:27 +0800

    Revert "win32: tune network performance"
    This reverts commit ab57e7d94ac4f0f39dad31d8c07f87c7b41ca2b6.
    
    See #49.

--- src/net/network.hpp
+++ src/net/network.hpp
@@ -68,10 +68,10 @@ const uint32_t kSpdyDisablePush = 0;
 // from net/spdy/spdy_session.h
 // If more than this many bytes have been read or more than that many
 // milliseconds have passed, return ERR_IO_PENDING from ReadLoop.
-#ifdef _WIN32
-const int kYieldAfterBytesRead = 384 * 1024;
-#else
+#if 0
 const int kYieldAfterBytesRead = 32 * 1024;
+#else
+const int kYieldAfterBytesRead = 64 * 1024;
 #endif
 const int kYieldAfterDurationMilliseconds = 20;
 
