7964968d8d347c907e79e2b170e5361ecdea496d
Keyue Hu <hukeyue@vip.163.com>
2026-03-16 11:49:30 +0800

    Revert "win: improve qpc measurement"
    This reverts commit ffe36ee487d58153507896c8d6a41d42185db52d.

--- src/core/utils_win.cpp
+++ src/core/utils_win.cpp
@@ -216,16 +216,10 @@ static uint64_t GetMonotonicTimeQPC() {
   // to microseconds *before* dividing by ticks-per-second.
   //
 
-#if _WIN32_WINNT >= 0x0600
-  double quad_part = ElapsedNanoseconds.QuadPart;
-  quad_part = quad_part * NS_PER_SECOND / Frequency.QuadPart;
-  return quad_part;
-#else
-  ElapsedNanoseconds.QuadPart *= MS_PER_SECOND;
-  ElapsedNanoseconds.QuadPart /= Frequency.QuadPart;
-  ElapsedNanoseconds.QuadPart *= 1000;
-  return ElapsedNanoseconds.QuadPart;
-#endif
+  auto seconds = ElapsedNanoseconds.QuadPart / Frequency.QuadPart;
+  auto fractions = ElapsedNanoseconds.QuadPart % Frequency.QuadPart;
+
+  return seconds * NS_PER_SECOND + fractions * NS_PER_SECOND / Frequency.QuadPart;
 }
 
 uint64_t GetMonotonicTime() {
