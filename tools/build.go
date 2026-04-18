224e35559fef57c6e5d70dfa29e2c6a6757cfb10
Keyue Hu <hukeyue@vip.163.com>
2026-04-18 15:35:11 +0800

    Revert "freebsd: use system zlib"
    This reverts commit 80d06f7ffe8d650ca09b8b9aaf738c1025b6910e.

--- tools/build.go
+++ tools/build.go
@@ -888,9 +888,6 @@ func buildStageGenerateBuildScript() {
 	}
 	cmakeArgs = append(cmakeArgs, "-DENABLE_LLD=on")
 	cmakeArgs = append(cmakeArgs, "-DUSE_ZLIB=on")
-	if systemNameFlag == "freebsd" {
-		cmakeArgs = append(cmakeArgs, "-DUSE_SYSTEM_ZLIB=on")
-	}
 	cmakeArgs = append(cmakeArgs, "-DUSE_JSONCPP=on")
 	cmakeArgs = append(cmakeArgs, "-DGUI=ON", "-DCLI=ON", "-DSERVER=ON", "-DBUILD_DYLIB=on")
 	if useStaticBuildFlag {
