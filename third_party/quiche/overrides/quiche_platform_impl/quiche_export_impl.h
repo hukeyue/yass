339d540ebc492573d381550bf95091820661ea37
Keyue Hu <hukeyue@vip.163.com>
2026-04-18 16:28:26 +0800

    quiche: make it component buildable
    exclude msvc build. we face some troubles with debug builds.

--- third_party/quiche/overrides/quiche_platform_impl/quiche_export_impl.h
+++ third_party/quiche/overrides/quiche_platform_impl/quiche_export_impl.h
@@ -10,8 +10,28 @@
 // These macros are documented in:
 // net/third_party/quiche/src/quiche/common/platform/api/quiche_export.h
 
-#define QUICHE_EXPORT_IMPL COMPONENT_EXPORT(QUICHE)
-#define QUICHE_EXPORT_PRIVATE_IMPL COMPONENT_EXPORT(QUICHE)
-#define QUICHE_NO_EXPORT_IMPL
+#if defined(_WIN32)
+#  if defined(QUICHE_BUILD_DLL)
+#     define QUICHE_EXPORT_IMPL __declspec(dllexport)
+#     define QUICHE_NO_EXPORT_IMPL
+#  elif defined(QUICHE_CONSUME_DLL)
+#     define QUICHE_EXPORT_IMPL __declspec(dllimport)
+#     define QUICHE_NO_EXPORT_IMPL
+#  else
+#    define QUICHE_EXPORT_IMPL
+#    define QUICHE_NO_EXPORT_IMPL
+#  endif
+#else
+#  if defined(QUICHE_BUILD_DLL)
+#     define QUICHE_EXPORT_IMPL __attribute__((visibility("default")))
+#     define QUICHE_NO_EXPORT_IMPL
+#  elif defined(QUICHE_CONSUME_DLL)
+#     define QUICHE_EXPORT_IMPL
+#     define QUICHE_NO_EXPORT_IMPL
+#  else
+#    define QUICHE_EXPORT_IMPL
+#    define QUICHE_NO_EXPORT_IMPL
+#  endif
+#endif  // defined(_WIN32)
 
 #endif  // NET_THIRD_PARTY_QUICHE_OVERRIDES_QUICHE_PLATFORM_IMPL_QUICHE_EXPORT_IMPL_H_
