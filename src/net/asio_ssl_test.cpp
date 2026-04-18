3f90ae6b7d9c47593ca5060c4e5a36270f0cb909
Keyue Hu <hukeyue@vip.163.com>
2026-04-18 18:04:46 +0800

    asio_ssl: change assertions to gtest style

--- src/net/asio_ssl_test.cpp
+++ src/net/asio_ssl_test.cpp
@@ -32,7 +32,7 @@ TEST(SSL_TEST, LoadBuiltinCaBundle) {
   bssl::UniquePtr<SSL_CTX> ssl_ctx;
   ssl_ctx.reset(::SSL_CTX_new(::TLS_client_method()));
   std::string ca_bundle_content;
-  CHECK_EQ(0, get_binary_ca_bundle(&ca_bundle_content));
+  ASSERT_EQ(0, get_binary_ca_bundle(&ca_bundle_content));
   ASSERT_FALSE(ca_bundle_content.empty());
   int result = load_ca_to_ssl_ctx_from_mem(ssl_ctx.get(), ca_bundle_content);
   ASSERT_NE(result, 0);
@@ -42,7 +42,7 @@ TEST(SSL_TEST, LoadSupplementaryCaBundle) {
   bssl::UniquePtr<SSL_CTX> ssl_ctx;
   ssl_ctx.reset(::SSL_CTX_new(::TLS_client_method()));
   std::string ca_content;
-  CHECK_EQ(0, get_binary_supplementary_ca_bundle(&ca_content));
+  ASSERT_EQ(0, get_binary_supplementary_ca_bundle(&ca_content));
   ASSERT_FALSE(ca_content.empty());
   int result = load_ca_to_ssl_ctx_from_mem(ssl_ctx.get(), ca_content);
   ASSERT_NE(result, 0);
