a0a44f5b6cd926cadb1bdffc20b84b3525c46539
Keyue Hu <hukeyue@vip.163.com>
2026-05-14 14:22:18 +0800

    deb: lookup missing symbols in yass-common package

--- tools/build.go
+++ tools/build.go
@@ -917,9 +917,6 @@ func buildStageGenerateBuildScript() {
 	}
 	cmakeArgs = append(cmakeArgs, "-DENABLE_LLD=on")
 	cmakeArgs = append(cmakeArgs, "-DUSE_ZLIB=on")
-	if useComponentBuildFlag && systemNameFlag == "android" {
-		cmakeArgs = append(cmakeArgs, "-DUSE_SYSTEM_ZLIB=on")
-	}
 	cmakeArgs = append(cmakeArgs, "-DUSE_JSONCPP=on")
 	cmakeArgs = append(cmakeArgs, "-DGUI=ON", "-DCLI=ON", "-DSERVER=ON", "-DBUILD_DYLIB=on")
 	if useStaticBuildFlag {
